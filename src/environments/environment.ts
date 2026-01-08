3c65953254479eddce462e9916246f1575bc8159
anzeljc7 <tilen.anzeljc7@gmail.com>
2026-01-08 18:12:25 +0100

    Fixed base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://hostflow.software/payment',
-  graphqlUrl: '/graphql'
+  graphqlUrl: 'https://hostflow.software/payment/graphql'
 };
