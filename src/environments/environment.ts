5a54088c24586df54cc1d9f32fc9b53e3a608716
anzeljc7 <tilen.anzeljc7@gmail.com>
2026-01-08 18:25:04 +0100

    srv path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://hostflow.software/payment',
-  graphqlUrl: 'https://hostflow.software/payment/graphql'
+  apiBaseUrl: 'https://hostflow.software/property',
+  graphqlUrl: 'https://hostflow.software/property/graphql'
 };
